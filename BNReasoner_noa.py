ff410e2e1f66772123280ea0601ec00020d6decf
Maike Nützel <63598855+NutzelM@users.noreply.github.com>
2021-12-03 15:35:33 +0100

    Add files via upload

---
+++ BNReasoner_noa.py
@@ -0,0 +1,21 @@
+from typing import Union
+from BayesNet import BayesNet
+
+
+class BNReasoner:
+    def __init__(self, net: Union[str, BayesNet]):
+        """
+        :param net: either file path of the bayesian network in BIFXML format or BayesNet object
+        """
+        if type(net) == str:
+            # constructs a BN object
+            self.bn = BayesNet()
+            # Loads the BN from an BIFXML file
+            self.bn.load_from_bifxml(net)
+        else:
+            self.bn = net
+file_path = "testing/lecture_example.BIFXML"
+network = BNReasoner(file_path)
+print(network)
+
+    # TODO: This is where your methods should go
